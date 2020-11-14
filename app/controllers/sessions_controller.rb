26c76baac181b188c100353e5b447b0cb1f75bbe
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-11-14 23:14:00 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,6 @@ class SessionsController < ApplicationController
     session[:omniauth_data] = request.env['omniauth.auth']
 
     # Ye olde redirect
-    redirect_to home_path
+    redirect_to root_path
   end
 end
